58aa24c96e937fcc02ec09b6e52599c3ddfeb136
Randie <srandall1213@gmail.com>
2022-08-22 22:25:35 -0600

    HTML routes done & css correctly linked

--- server.js
+++ server.js
@@ -15,12 +15,15 @@ app.use(express.static('public'));
 
 //HTML ROUTES//
 
-//GET /notes returns notes.html 
-app.get('/notes', (req, res) => {
+//GET '/notes' returns notes.html 
+app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/notes.html"))
 });
 
 //GET * returns index.html
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"))
+})
 
 //API ROUTES//
 
