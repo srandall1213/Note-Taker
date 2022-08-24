2b2c8035a38dea67e1b2d3583385a61998921a80
Randie <srandall1213@gmail.com>
2022-08-24 09:41:20 -0600

    refreshed db

--- server.js
+++ server.js
@@ -16,6 +16,16 @@ app.use(express.urlencoded({ extended: true }));
 //Middleware for the static public files
 app.use(express.static('public'));
 
+// GET Route returns homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+//GET "/notes" returns notes.html
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
+});
+
 // Promise version of fs.readFile
 const readFromFile = util.promisify(fs.readFile);
 
@@ -27,7 +37,7 @@ const writeToFile = (destination, content) =>
     err ? console.error(err) : console.info(`\nData written to ${destination}`)
   );
 
-//Read & Append Function
+//Append Function
 const appendNote = (content, file) => {
   fs.readFile(file, 'utf8', (err, data) => {
     if (err) {
@@ -42,8 +52,9 @@ const appendNote = (content, file) => {
 
 //GET /api/notes should read the db.json file and return all saved notes as JSON
 app.get("/api/notes", (req, res) => {
-  console.info(`${req.method} request received for notes`);
+  console.info(`${req.method} request received to add a note`);
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+  
 })
 
 //POST /api/notes should receive a new note to save on the request body, add it to the db.json file, 
@@ -62,30 +73,23 @@ app.post('/api/notes', (req, res) => {
     };
 
     appendNote(newNote, './db/db.json');
-    return newNote;
+
+    res.json(`Note added successfully 📝`);
     
   } else {
     res.error('Error in adding note');
   }
 });
 
-//HTML ROUTES//
-
-//GET "/notes" returns notes.html 
-app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/notes.html"))
-});
-
-//GET "*" returns index.html
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/index.html"))
-})
- 
 //DELETE /api/notes/:id should receive a query parameter that contains the id of a note to delete. 
 //To delete a note, you'll need to read all notes from the db.json file, remove the note with the 
 //given id property, and then rewrite the notes to the db.json file.
 
-
+//GET wildcard returns index.html
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"))
+});
+ 
 //Listening
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🙌`)
